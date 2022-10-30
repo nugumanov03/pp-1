81ed31ca3cd6d4aae088a94f535a7bdf4a7f363a
Kiperlot <you@example.com>
2022-10-30 16:57:44 +0600

    Test2

---
+++ lessons_g3/pp_g2/a.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    char a[4][4];
+    for(int i = 0;i < 4;i++){
+        for(int j = 0; j < 4;j++){
+            cin >> a[i][j];
+        }
+    }
+    // ai
+    bool flag = true;
+    for(int i = 0;i < 4;i++){
+        for(int j = 0;j < 4;j++){
+            if(a[i][j] == a[i+1][j] && a[i][j] == a[i][j+1] && a[i][j]== a[i+1][j+1]){
+                flag = false;
+                break;
+            }
+        }
+    }
+    if(flag){
+        cout << "Yes";
+    }else{
+        cout << "No";
+    }
+
+    return 0;
+}
