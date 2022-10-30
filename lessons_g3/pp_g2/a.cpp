6a4a8275120bc35c8b82f20d46168cf67737a740
Kiperlot <you@example.com>
2022-10-30 16:58:55 +0600

    Test2

--- lessons_g3/pp_g2/a.cpp
+++ lessons_g3/pp_g2/a.cpp
@@ -11,8 +11,8 @@ int main(){
     }
     // ai
     bool flag = true;
-    for(int i = 0;i < 4;i++){
-        for(int j = 0;j < 4;j++){
+    for(int i = 0;i < 3;i++){
+        for(int j = 0;j < 3;j++){
             if(a[i][j] == a[i+1][j] && a[i][j] == a[i][j+1] && a[i][j]== a[i+1][j+1]){
                 flag = false;
                 break;
