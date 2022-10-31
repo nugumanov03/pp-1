2bb49e2413159fe3bd3beb61a217feaa4704642d
Kiperlot <you@example.com>
2022-10-31 20:00:15 +0600

    Strings

---
+++ lessons-g1/pp_4/e.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+
+    int was[128];
+    for(int i = 0;i < 128;i++){
+        was[i] = 0;
+    }
+    int nums[s.size()];
+    // for(int i = 0; i < s.size();i++){
+    //     nums[i] = 
+    // }
+    for(int i = 0;i < s.size() ; i++){
+        int num = (int)s[i]; 
+        // cout << s[i];
+        was[num]++;
+    }
+    for(int i = 0; i < 128;i++){
+        if(was[i] == 2){
+            cout << (char)i;
+        }
+        // cout << was[i] << " ";
+    }
+
+
+    return 0;
+}
