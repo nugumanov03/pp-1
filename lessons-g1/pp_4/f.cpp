0667eb2f7568d4aa5880c8fc5256397e91f7a578
Kiperlot <you@example.com>
2022-10-31 20:26:06 +0600

    Strings

--- lessons-g1/pp_4/f.cpp
+++ lessons-g1/pp_4/f.cpp
@@ -6,20 +6,34 @@ int main(){
     string s , s2;
     cin >> s2 >> s;
     bool flag = false;
-    for(int i = 0;i < s.size();i++){
-        int cnt = 0;
-        for(int j = 0;j < s2.size() ; j++){
-            if(s[i+j] == s2[j]){
-                cnt++;
-            }
-            if(cnt == s2.size()){
-                flag = true;
-                break;
-            }
+    // for(int i = 0;i < s.size();i++){
+    //     int cnt = 0;
+    //     for(int j = 0;j < s2.size() ; j++){
+    //         if(s[i+j] == s2[j]){
+    //             cnt++;
+    //         }
+    //         if(cnt == s2.size()){
+    //             flag = true;
+    //             break;
+    //         }
+    //     }
+    //     if(flag){
+    //         break;
+    //     }
+    // }
+int k = 0;
+    for(int i = 0; i < s.size();i++){
+        if(s[i] == s2[k]){
+            k++;
+        }else{
+            i-=k;
+            k = 0;
         }
-        if(flag){
+        if(k == s2.size() ){
+            flag =true;
             break;
         }
+        // cout << k << " ";
     }
 
     if(flag){
