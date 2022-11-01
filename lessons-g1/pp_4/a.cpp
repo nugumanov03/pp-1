1ac0969cd3da3c35b636f5b46c0396107d021eb6
Kiperlot <you@example.com>
2022-11-01 12:05:43 +0600

    Update

--- lessons-g1/pp_4/a.cpp
+++ lessons-g1/pp_4/a.cpp
@@ -7,13 +7,21 @@ int main(){
     char a = 'F';
     // int a_num = a;
 
-    int a_num = (char)'F';
+    int a_num = (int)'F';
+
+    char a_ch = (char)70;
 
     cout << a_num;
     char b = 'C';
     string s;
-
-
+    cin >> s; // s = "ABCD"
+    // s.size() : 4
+    for(int i = 0 ; i < s.size();i++){
+        s[i]; // char 
+        int num = s[i];
+        s[i] = (char)(num+1);
+    }
+    
 
 
 }
