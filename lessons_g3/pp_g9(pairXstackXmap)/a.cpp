7dc7b0ca47d9bdaf27021dfa072429862c8acf64
Kiperlot <you@example.com>
2022-11-26 16:24:01 +0600

    26.11

---
+++ lessons_g3/pp_g9(pairXstackXmap)/a.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<map>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector <pair <int , int> > vt;
+    for(int i =0 ;i < n;i++){
+        pair <int , int> pr;
+        cin >> pr.first >> pr.second;       
+        // pr = <x , y>
+        vt.push_back(pr);
+    }
+    sort(vt.begin() , vt.end());
+    for(int i = 0; i < vt.size();i++){
+        cout << vt[i].first << " " << vt[i].second << endl; 
+
+    }
+
+    return 0;
+}
