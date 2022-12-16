6cb5dd6d12a4ebdd87bd881feed289d035d664b0
Kiperlot <you@example.com>
2022-12-16 16:41:36 +0600

    16.12

---
+++ gits.cpp
@@ -0,0 +1,13 @@
+/*
+
+https://github.com/Kilimanjaroooo * Anuar 1
+
+https://github.com/dilnaz00 * Dilnaz 3
+
+https://github.com/moonaimaa * Aisha 3
+
+https://github.com/enaassk * Altynai 1
+
+
+
+*/
